2321d3fb03b39f84177041ae5db12be065cbbed3
Pedro Henrique Oliveira Bezerra <pedro.11hob@gmail.com>
2026-04-08 20:11:08 -0300

    Realizando o incremento do java script com o html

---
+++ main.js
@@ -0,0 +1,17 @@
+// Cria função para validar preenchimento de campos da tela de login
+
+function acessar() {
+
+    let loginEmail = document.getElementById('login-Email').value;
+    let loginSenha = document.getElementById('login-Senha').value;
+
+    // Estrutura que verifica se os campos foram preenchidos
+
+    if (!login-Senha || !loginEmail) {
+
+        alert("Por favor, preencha todos os campos!");
+
+    }
+
+
+}
